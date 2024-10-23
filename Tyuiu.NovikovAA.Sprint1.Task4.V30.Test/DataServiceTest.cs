cb8a5f8afda149f8e3e2797dc9bbda4d9631075c
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-10-23 22:21:43 +0600

    Task4

--- Tyuiu.NovikovAA.Sprint1.Task4.V30.Test/DataServiceTest.cs
+++ Tyuiu.NovikovAA.Sprint1.Task4.V30.Test/DataServiceTest.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.NovikovAA.Sprint1.Task4.V30.Test
             double x = 2;
             double y = 2;
             double e = 2;
-            double wait = 2;
+            double wait = 1.9;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
