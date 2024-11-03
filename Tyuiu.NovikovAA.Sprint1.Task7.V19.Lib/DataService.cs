ff351b01edcb4fe691b7e6d6dabaf4d4f0d94da0
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-04 00:00:56 +0600

    Task7

--- Tyuiu.NovikovAA.Sprint1.Task7.V19.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint1.Task7.V19.Lib/DataService.cs
@@ -8,8 +8,6 @@ namespace Tyuiu.NovikovAA.Sprint1.Task7.V19.Lib
         public double Calculate(double x)
         {
             double res = x - (7 * Math.Pow(x, 2) / Math.Pow(x, 3)) + Math.Sin(x) + Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5));
-            if (x > 5);
-            return 
             return Math.Round(res, 3);
         }
     }
